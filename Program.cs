5cd78712bcb9a700f15d39f2b68a77bdfd467f86
Romraev <romraev@yandex.ru>
2023-03-21 18:46:27 +0300

    task 1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Подготовил Раевский Р.Ю.
+
+// Task 19.
+
+int Grade(int a, int b)
+{
+    int grad = 1;
+    for (int i = 1; i<= b; i++)
+    {
+       grad = grad * a;
+    }
+    return grad;
+}
+
+Console.Write("Enter number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (B == 0) Console.WriteLine ($"Number {A} in grade {B} equals 1 ");
+else Console.WriteLine($"Number {A} in grade {B} equals {Grade(A, B)}");
