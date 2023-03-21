3ecf5dae7fa9d23cd904d36cb63350d81523fefd
Romraev <romraev@yandex.ru>
2023-03-21 19:11:33 +0300

    task 3

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ else Console.WriteLine($"Number {A} in grade {B} equals {Grade(A, B)}");
 
 // Task 27.
 
-int SUM(int num)
+/* int SUM(int num)
 {
     int sum = 0;
     while (num > 0)
@@ -39,3 +39,37 @@ Console.Write("Enter your number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"Sum of digits in number {num} equals {SUM(num)}");
+*/
+
+//Task 29.
+
+int[] CreateRandomArray(int size, int minvalue, int maxvalue)
+{
+    int[] array = new int[size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minvalue, maxvalue + 1);
+    }
+    return array;
+}
+
+void WriteArray(int[]array)
+{
+    for(int i=0; i<array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+Console.Write("Input a length of array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input min value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input min value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(length, min, max);
+WriteArray(myArray);
